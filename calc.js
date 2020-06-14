2bc1dbe55c3accf995e0b1cf61499a7e19ba3c73
chbailey081 <39930155+chbailey081@users.noreply.github.com>
2020-06-14 10:13:21 -0400

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,12 @@
+alert("connected");
+
+var age = prompt("what is your age?")
+var days = age * 365.25;
+var months = age * 12;
+var hours = age * 365.25 * 24;
+var minutes = age * 365.25 * 525600;
+alert(age = " years is roughly " + days + " days");
+alert(months = " you are roughly " + months + " months old");
+alert(hours = " which is roughly " + hours + " hours");
+alert(minutes = " and is also roughly " + minutes + " minutes");
+
